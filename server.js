49ab9ba1d7ef259c38fbd58643551363a98e7689
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-07-07 20:23:59 +0530

    Updated server.js and removed vercel.json

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const path = require("path");
+const express=require('express')
+const cors=require('cors')
+const dotenv=require('dotenv')
+const app=express()
+const path=require('path')
 
-const app = express();
-dotenv.config();
+dotenv.config()
 
-app.use(
-  cors({
-    origin: ["https://my-portfolio-website-steel-eight.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-app.use(express.json());
+//middlewares
+app.use(cors())
+app.use(express.json())
 
-app.use("/api/v1/portfolio", require("./routes/PortfolioRoute"));
+//static files
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-// Export the Express API
-module.exports = app;
+//Routes
+app.use('/api/v1/portfolio', require('./routes/PortfolioRoute'))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
+
+//port
+const port =process.env.PORT || 8080
+
+//listen
+app.listen(port,()=>{
+    console.log(`server is listening on PORT ${port}` )
+})
