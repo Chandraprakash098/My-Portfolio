91786350742476e3e44c4fbc9b252288682ecb60
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-07-07 20:45:01 +0530

    Update package.json and server.js

--- server.js
+++ server.js
@@ -3,8 +3,34 @@ const cors=require('cors')
 const dotenv=require('dotenv')
 const app=express()
 const path=require('path')
+const fs = require("fs");
 
 dotenv.config()
+
+const projectRoot = path.resolve(__dirname);
+const frontendBuildPath = path.join(projectRoot, "frontend", "build");
+
+console.log("Project root:", projectRoot);
+console.log("Frontend build path:", frontendBuildPath);
+console.log(
+  "Frontend build directory exists?",
+  fs.existsSync(frontendBuildPath)
+);
+console.log("Contents of project root:", fs.readdirSync(projectRoot));
+
+if (fs.existsSync(frontendBuildPath)) {
+  console.log(
+    "Contents of frontend build directory:",
+    fs.readdirSync(frontendBuildPath)
+  );
+}
+
+// Use the resolved path in your Express setup
+app.use(express.static(frontendBuildPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendBuildPath, "index.html"));
+});
 
 //middlewares
 app.use(cors())
