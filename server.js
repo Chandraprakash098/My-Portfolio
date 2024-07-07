9bab78782f4d5881e8eaa913baff52438505162f
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-07-07 19:52:03 +0530

    Update server.js for backend configuration

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ const path = require("path");
 const app = express();
 dotenv.config();
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://my-portfolio-website-steel-eight.vercel.app/"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+
+  })
+);
 app.use(express.json());
 
 // Static files
-app.use(express.static(path.join(__dirname, "./frontend/build")));
+// app.use(express.static(path.join(__dirname, "./frontend/build")));
 
 // Routes
 app.use("/api/v1/portfolio", require("./routes/PortfolioRoute"));
 
 // Serve index.html for any other routes (React Router routes)
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+// });
 
 // Port
 const port = process.env.PORT || 8080;
