87553af1775565171955de054c9a592c8a443c09
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-07-07 20:13:57 +0530

    Update package.json, server.js, add vercel.json for deployment

--- server.js
+++ server.js
@@ -5,32 +5,17 @@ const path = require("path");
 
 const app = express();
 dotenv.config();
-// Middlewares
+
 app.use(
   cors({
-    origin: ["https://my-portfolio-website-steel-eight.vercel.app/"],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true
-
+    origin: ["https://my-portfolio-website-steel-eight.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 app.use(express.json());
 
-// Static files
-// app.use(express.static(path.join(__dirname, "./frontend/build")));
-
-// Routes
 app.use("/api/v1/portfolio", require("./routes/PortfolioRoute"));
 
-// Serve index.html for any other routes (React Router routes)
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-// });
-
-// Port
-const port = process.env.PORT || 8080;
-
-// Start server
-app.listen(port, () => {
-  console.log(`Server is listening on PORT ${port}`);
-});
+// Export the Express API
+module.exports = app;
