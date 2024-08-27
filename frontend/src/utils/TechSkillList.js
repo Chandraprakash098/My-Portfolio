0a8b09739beafac13187cbfd8da1edc7f718657f
user.name like:"Chandra Prakash <user.email like:"chandraprakash96386@gmail.com>
2024-08-27 23:54:45 +0530

    update TechSkillList

--- frontend/src/utils/TechSkillList.js
+++ frontend/src/utils/TechSkillList.js
@@ -34,61 +34,71 @@ export const TechSkillList = [
   },
   {
     _id: 4,
-    name: "JavaScript",
-    icon: FaJsSquare,
-  },
-  {
-    _id: 5,
-    name: "React JS",
-    icon: FaReact,
-  },
-  {
-    _id: 6,
-    name: "Tailwind",
-    icon: SiTailwindcss,
-  },
-  {
-    _id: 7,
-    name: "React MUI",
-    icon: FaReact,
-  },
-  {
-    _id: 8,
-    name: "Node JS",
-    icon: FaNodeJs,
-  },
-  {
-    _id: 9,
-    name: "Express JS",
-    icon: SiExpress,
-  },
-  {
-    _id: 10,
-    name: "MongoDB",
-    icon: SiMongodb,
-  },
-  {
-    _id: 11,
-    name: "Next.JS",
-    icon: FaReact,
-  },
-  {
-    _id: 12,
-    name: "Git / GitHub",
-    icon: FaGithub,
-  },
-  {
-    _id: 13,
-    name: "Redux",
-    icon: TbBrandRedux,
-  },
-  {
-    _id: 14,
     name: "Python",
     icon: FaPython,
   },
   {
+    _id: 5,
+    name: "JavaScript",
+    icon: FaJsSquare,
+  },
+  {
+    _id: 6,
+    name: "React JS",
+    icon: FaReact,
+  },
+  {
+    _id: 7,
+    name: "Tailwind",
+    icon: SiTailwindcss,
+  },
+  {
+    _id: 8,
+    name: "React MUI",
+    icon: FaReact,
+  },
+  {
+    _id: 9,
+    name: "Node JS",
+    icon: FaNodeJs,
+  },
+  {
+    _id: 10,
+    name: "Express JS",
+    icon: SiExpress,
+  },
+  {
+    _id: 11,
+    name: "MongoDB",
+    icon: SiMongodb,
+  },
+  {
+    _id: 12,
+    name: "MySql",
+    icon: SiMongodb,
+  },
+  {
+    _id: 13,
+    name: "Next.JS",
+    icon: FaReact,
+  },
+  {
+    _id: 14,
+    name: "Django",
+    icon: FaReact,
+  },
+  {
     _id: 15,
+    name: "Git / GitHub",
+    icon: FaGithub,
+  },
+  {
+    _id: 16,
+    name: "Redux",
+    icon: TbBrandRedux,
+  },
+  {
+    _id: 17,
     name: "Photoshop",
     icon: SiAdobephotoshop,
   },
